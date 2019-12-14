5fd669c34495bc37a59511e7b9a071268c23a547
HuimaHeimo <46885479+HuimaHeimo@users.noreply.github.com>
2019-12-14 12:01:15 +0200

    Pohja REST API:lle

---
+++ REST API/controller.js
@@ -0,0 +1,134 @@
+var mysql = require('mysql');
+var sqlQuery;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',  // HUOM! Älä käytä root:n tunnusta tuotantokoneella!!!!
+    password: 'Kissa123',
+    database: 'asiakas'
+});
+
+let users = {
+    userName: "Testi",
+    loginTime: Date.now(),
+    sessionId: 1234
+};
+
+module.exports =
+    {
+        fetchTypes: function (req, res) {
+            connection.query('SELECT * FROM Asiakastyyppi', function (error, results, fields) {
+                if (error) {
+                    console.log("Virhe haettaessa dataa Asiakas-taulusta, syy: " + error);
+                    //res.send(error);
+                    //res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+                    res.send({ "status": 500, "error": error, "response": null });
+                }
+                else {
+                    console.log("Data = " + JSON.stringify(results));
+                    res.json(results);
+                    //res.statusCode = 201;
+                    //res.send(results);
+                    //res.send({ "status": 768, "error": null, "response": results });
+                }
+            });
+
+    },
+
+    login: function (request, response) {
+        users.userName = request.body.username;
+        response.cookie("userData", users);
+        response.redirect("/");
+    },
+
+
+        fetchAll: function (req, res) {
+            if (req.query.NIMI == "" && req.query.OSOITE == "" && req.query.ASTY_AVAIN == "") {
+                sqlQuery = "SELECT * FROM asiakas";
+            }
+            else {
+                sqlQuery = "SELECT * FROM asiakas WHERE NIMI LIKE '" + req.query.NIMI + "%' " +
+                    "AND OSOITE LIKE '" + req.query.OSOITE + "%' AND ASTY_AVAIN LIKE '" + req.query.ASTY_AVAIN + "%'";
+            }
+            connection.query(sqlQuery, function (error, results, fields) {
+                if (error) {
+                    console.log("Virhe haettaessa dataa Asiakas-taulusta, syy: " + error);
+                    //res.send(error);
+                    //res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+                    res.send({ "status": 500, "error": error, "response": null });
+                }
+                else {
+                    console.log("Data = " + JSON.stringify(results));
+                    console.log("Params = " + JSON.stringify(req.query));
+                    res.json(results);
+                    //res.statusCode = 201;
+                    //res.send(results);
+                    //res.send({ "status": 768, "error": null, "response": results });
+                }
+            });
+        },
+
+        create: function (req, res) {
+            if (req.query.NIMI === "" || req.query.OSOITE == "" || req.query.ASTY_AVAIN == "" || req.query.POSTINRO == "" || req.query.POSTITMP == "" || req.query.LUONTIPVM == "") {
+                console.log("Virhe lisattaessa uutta asiakasta, tayta kaikki kentat");
+                //res.send(error);
+                //res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+                res.send({ "status": 500, "error": error, "response": null });
+                
+            }
+            else {
+                sqlQuery = "INSERT INTO asiakas (NIMI, OSOITE, POSTINRO, POSTITMP, LUONTIPVM, ASTY_AVAIN)" + " " +
+                    "VALUES" + " " + "(" + "'" + req.query.NIMI + "'" + "," + "'" + req.query.OSOITE + "'" + "," + "'" + req.query.POSTINRO +
+                    "'" + "," + "'" + req.query.POSTITMP + "'" + "," + "'" + req.query.LUONTIPVM + "'" + "," + "'" + req.query.ASTY_AVAIN + "'" + ")" + ";";
+
+                //sqlQuery = "SELECT * FROM asiakas WHERE NIMI LIKE '" + req.query.NIMI + "%' " +
+                //    "AND OSOITE LIKE '" + req.query.OSOITE + "%' AND ASTY_AVAIN LIKE '" + req.query.ASTY_AVAIN + "%'";
+            }
+            connection.query(sqlQuery, function (error, results, fields) {
+                if (error) {
+                    console.log("Virhe lisattaessa uutta asiakasta, syy: " + error);
+                    window.alert(error);
+                    //res.send(error);
+                    //res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+                    window.alert(error);
+                    res.send({ "status": 500, "error": error, "response": null });
+                    
+                }
+
+                console.log("Data = " + JSON.stringify(results));
+                console.log("Params = " + JSON.stringify(req.query));
+                res.json(results);
+            });
+        },
+
+        update: function (req, res) {
+
+        },
+
+        delete: function (req, res) {
+            if (req.query.ID == "") {
+                
+            }
+            else {
+                sqlQuery = "DELETE FROM asiakas WHERE AVAIN=" + "'" + req.params.id + "'" + ";";
+            }
+            connection.query(sqlQuery, function (error, results, fields) {
+                if (error) {
+                    console.log("Virhe haettaessa dataa Asiakas-taulusta, syy: " + error);
+                    //res.send(error);
+                    //res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+                    res.send({ "status": 500, "error": error, "response": null });
+                }
+                else {
+                    console.log("Data = " + JSON.stringify(results));
+                    console.log("Params = " + JSON.stringify(req.query));
+                    res.json(results);
+                    //res.statusCode = 201;
+                    //res.send(results);
+                    //res.send({ "status": 768, "error": null, "response": results });
+                }
+            });
+
+
+        }
+    }
---
+++ REST API/server.js
@@ -0,0 +1,117 @@
+
+var express = require('express');
+var app=express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var customerController = require('./controller');
+var cookieParser = require('cookie-parser');
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3002;
+
+let users = {
+    userName: "Testi",
+    loginTime: Date.now(),
+    sessionId: 1234
+};
+
+
+
+// Määrittelee esimerkiksi ulkoiset tyylitiedostot ja muut tiedostot, joita voi käyttää selaimen kautta.
+app.use(express.static(__dirname + '/public'));
+
+
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
+
+// Staattiset filut
+// app.use(express.static('public'));
+
+
+// REST API Asiakas
+app.route('/Types')
+    .get(customerController.fetchTypes);
+
+
+app.route('/login')
+    .post(customerController.login);
+   
+
+
+
+app.route('/Asiakas')
+    .get(customerController.fetchAll)
+    .post(customerController.create);
+
+app.route('/Asiakas/:id')
+    .put(customerController.update)
+    .delete(customerController.delete);
+
+
+app.get('/getuser', (req, res) => {
+    console.log(req.cookies);
+    res.send(req.cookies)
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie("userData");
+    res.send("Olet kirjautunut ulos")
+});
+
+app.get('/userdata', function (request, response) {
+    response.send(request.cookies);
+});
+
+
+
+
+app.get('/', function (request, response) {
+
+    if (request.cookies.userData == null) {
+        response.redirect("/login");
+    }
+    else {
+        fs.readFile("front.html", function (err, data) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
+            response.end();
+        });
+    }
+        
+    
+});
+
+app.get('/login', function (request, response, req) {
+    if (request.cookies.userData == null) {
+        fs.readFile("login.html", function (err, data) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
+            response.end();
+        });
+    }
+    response.cookie("userData", users);
+
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running AT http://${hostname}:${port}/`);
+});
+
